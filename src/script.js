53659d9df4b987cefce30cbe89a436c2366a9fdf
PxMach <moundjidmachghour58@gmail.com>
2025-05-30 15:09:24 +0100

    Ajout de la fonctionnalité de diaporama avec gestion des images pour mobile et desktop, ainsi que la navigation entre les diapositives via des boutons. Mise à jour dynamique de l'arrière-plan en fonction de la taille de l'écran.

--- src/script.js
+++ src/script.js
@@ -3,10 +3,15 @@ const h2 = document.querySelector("h2");
 const para = document.querySelector(".para");
 const buttonLeft = document.querySelector(".button-left");
 const buttonRight = document.querySelector(".button-right");
+const header = document.querySelector("header");
 
 const nav = document.querySelector("nav");
 const hamburger = document.querySelector(".hamburger");
 const clos = document.querySelector(".clos");
+
+let currentSlide = 1;
+let isMobile = window.innerWidth <= 376;
+let currentImage;
 
 const slides = [
    {
@@ -16,6 +21,8 @@ const slides = [
             in bringing your vision to life. Create a room in your own style
             with our collection and make your property a reflection of you
             and what you love.`,
+      desktopImage: "../images/desktop-image-hero-1.jpg",
+      mobileImage: "../images/mobile-image-hero-1.jpg",
    },
    {
       title: "We are available all across the globe",
@@ -23,6 +30,8 @@ const slides = [
             for your home or place of business. Locally, we're in most major
             cities throughout the country. Find the branch nearest you using our
             store locator. Any questions? Don't hesitate to contact us today.`,
+      desktopImage: "../images/desktop-image-hero-2.jpg",
+      mobileImage: "../images/mobile-image-hero-2.jpg",
    },
    {
       title: "Manufactured with the best materials",
@@ -31,12 +40,40 @@ const slides = [
             product is made as perfect and as consistent as possible. With three
             decades of experience in this industry, we understand what customers
             want for their home and office.`,
+      desktopImage: "../images/desktop-image-hero-3.jpg",
+      mobileImage: "../images/mobile-image-hero-3.jpg",
    },
 ];
 
-// il reste 
+function updateSlide() {
+   h2.textContent = slides[currentSlide - 1].title;
+   para.textContent = slides[currentSlide - 1].content;
 
+   if (isMobile) {
+      currentImage = slides[currentSlide - 1].mobileImage;
+   } else {
+      currentImage = slides[currentSlide - 1].desktopImage;
+   }
+   header.style.backgroundImage = `url(${currentImage})`;
+}
 
+// Détecter les changements de taille d'écran
+window.addEventListener("resize", () => {
+   isMobile = window.innerWidth <= 376;
+   updateSlide();
+});
+
+buttonRight.addEventListener("click", () => {
+   currentSlide = currentSlide === slides.length ? 1 : currentSlide + 1;
+   updateSlide();
+});
+
+buttonLeft.addEventListener("click", () => {
+   currentSlide = currentSlide === 1 ? slides.length : currentSlide - 1;
+   updateSlide();
+});
+
+// navigation
 function useNav() {
    // open menu
    hamburger.addEventListener("click", () => {
